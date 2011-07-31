705e51b16c4e0cf532af73d3e75a072db935b9a2
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2011-08-01 02:36:04 +0900

    []=と[]の本実装
    - 三角測量

--- lib/kvs.rb
+++ lib/kvs.rb
@@ -1,7 +1,13 @@
 class Kvs
+  def initialize
+    @records = {}
+  end
+
   def []=(key,value)
+    @records[key] = value
   end
 
   def [](key)
+    @records[key]
   end
 end
--- spec/kvs_spec.rb
+++ spec/kvs_spec.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require_relative File.join('..','lib','kvs')
 
 describe Kvs do
@@ -7,7 +8,18 @@ describe Kvs do
   end
 
   describe '[]' do
-    subject { Kvs.new[:key] }
-    it { should be_nil }
+    context 'まだ何も登録されていない場合' do
+      subject { Kvs.new[:key] }
+      it { should be_nil }
+    end
+
+    context ':keyで値が登録されている場合' do
+      before do
+        @kvs = Kvs.new
+        @kvs[:key] = 'value'
+      end
+      subject { @kvs }
+      its([:key]) { should eq 'value' }
+    end
   end
 end
